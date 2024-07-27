afa5a233fbf693726974dcd0665518dc0f9450fd
haldaniko <haldaniko@gmail.com>
2024-07-27 20:26:44 +0200

    include 'station_api.urls' to urlpatterns

--- train_station/urls.py
+++ train_station/urls.py
@@ -4,4 +4,5 @@ from django.urls import path, include
 urlpatterns = [
     path('admin/', admin.site.urls),
     path("api/user/", include("user.urls", namespace="user")),
+    path("api/station/", include("station_api.urls", namespace="station")),
 ]
