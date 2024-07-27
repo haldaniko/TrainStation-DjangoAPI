b0813d83c321a47155b5fff04f562e05120cee06
haldaniko <haldaniko@gmail.com>
2024-07-27 14:27:06 +0200

    Create 'Crew', 'Station', 'Route', 'Train', 'TrainType', 'Order', 'Ticket' and 'Journey' models

--- station_api/models.py
+++ station_api/models.py
@@ -1,3 +1,50 @@
 from django.db import models
 
-# Create your models here.
+from user.models import User
+
+
+class Crew(models.Model):
+    first_name = models.CharField(max_length=255)
+    last_name = models.CharField(max_length=255)
+
+
+class Station(models.Model):
+    name = models.CharField(max_length=255)
+    latitude = models.FloatField()
+    longitude = models.FloatField()
+
+
+class Route(models.Model):
+    source = models.ForeignKey(Station, on_delete=models.CASCADE, related_name='route_source')
+    destination = models.ForeignKey(Station, on_delete=models.CASCADE, related_name='route_destination')
+    distance = models.IntegerField()
+
+
+class TrainType(models.Model):
+    name = models.CharField(max_length=255)
+
+
+class Train(models.Model):
+    name = models.CharField(max_length=255)
+    cargo_num = models.IntegerField()
+    places_in_cargo = models.IntegerField()
+    train_type = models.ForeignKey(TrainType, on_delete=models.CASCADE)
+
+
+class Journey(models.Model):
+    route = models.ForeignKey(Route, on_delete=models.CASCADE)
+    train = models.ForeignKey(Train, on_delete=models.CASCADE)
+    departure_time = models.DateTimeField()
+    arrival_time = models.DateTimeField()
+
+
+class Order(models.Model):
+    created_at = models.DateTimeField()
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+
+
+class Ticket(models.Model):
+    cargo = models.IntegerField()
+    seat = models.IntegerField()
+    journey = models.ForeignKey(Journey, on_delete=models.CASCADE)
+    order = models.ForeignKey(Order, on_delete=models.CASCADE)
