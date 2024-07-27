ca046b96543bbd7fdb61f041c2bd65865f98e95c
haldaniko <haldaniko@gmail.com>
2024-07-27 20:28:44 +0200

    Create ViewSets for all models; Add serializer branching for RouteViewSet

--- station_api/views.py
+++ station_api/views.py
@@ -1,3 +1,71 @@
-from django.shortcuts import render
+from rest_framework import viewsets
+from .models import (
+    Crew,
+    Station,
+    Route,
+    Train,
+    TrainType,
+    Order,
+    Ticket,
+    Journey
+)
+from .serializers import (
+    CrewSerializer,
+    StationSerializer,
+    TrainSerializer,
+    TrainTypeSerializer,
+    OrderSerializer,
+    TicketSerializer,
+    JourneySerializer,
+    RouteSerializer,
+    RouteListSerializer,
+    RouteDetailSerializer,
+)
 
-# Create your views here.
+
+class CrewViewSet(viewsets.ModelViewSet):
+    queryset = Crew.objects.all()
+    serializer_class = CrewSerializer
+
+
+class StationViewSet(viewsets.ModelViewSet):
+    queryset = Station.objects.all()
+    serializer_class = StationSerializer
+
+
+class RouteViewSet(viewsets.ModelViewSet):
+    queryset = Route.objects.all()
+
+    def get_serializer_class(self):
+        if self.action == "list":
+            return RouteListSerializer
+
+        if self.action == "retrieve":
+            return RouteDetailSerializer
+
+        return RouteSerializer
+
+
+class TrainTypeViewSet(viewsets.ModelViewSet):
+    queryset = TrainType.objects.all()
+    serializer_class = TrainTypeSerializer
+
+
+class TrainViewSet(viewsets.ModelViewSet):
+    queryset = Train.objects.all()
+    serializer_class = TrainSerializer
+
+
+class JourneyViewSet(viewsets.ModelViewSet):
+    queryset = Journey.objects.all()
+    serializer_class = JourneySerializer
+
+
+class OrderViewSet(viewsets.ModelViewSet):
+    queryset = Order.objects.all()
+    serializer_class = OrderSerializer
+
+
+class TicketViewSet(viewsets.ModelViewSet):
+    queryset = Ticket.objects.all()
+    serializer_class = TicketSerializer
