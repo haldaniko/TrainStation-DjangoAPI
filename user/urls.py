1d7dc7c5b081f310761832014828f2af3c89e1c2
haldaniko <haldaniko@gmail.com>
2024-07-27 14:00:00 +0200

    add 'register', 'login' and 'me' routes

---
+++ user/urls.py
@@ -0,0 +1,10 @@
+from django.urls import path
+from user.views import CreateUserView, CreateTokenView, ManageUserView
+
+app_name = "user"
+
+urlpatterns = [
+    path("register/", CreateUserView.as_view(), name="create"),
+    path("login/", CreateTokenView.as_view(), name="login"),
+    path("me/", ManageUserView.as_view(), name="manage"),
+]
