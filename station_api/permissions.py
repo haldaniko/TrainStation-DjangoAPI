b988c7286b9717ca153c568ca704e9f107b9b8e1
haldaniko <haldaniko@gmail.com>
2024-07-29 16:17:13 +0200

    implement IsAdminOrIfAuthenticatedReadOnly(BasePermission) class

---
+++ station_api/permissions.py
@@ -0,0 +1,13 @@
+from rest_framework.permissions import SAFE_METHODS, BasePermission
+
+
+class IsAdminOrIfAuthenticatedReadOnly(BasePermission):
+    def has_permission(self, request, view):
+        return bool(
+            (
+                request.method in SAFE_METHODS
+                and request.user
+                and request.user.is_authenticated
+            )
+            or (request.user and request.user.is_staff)
+        )
