16f37bed5be576320d8b9a3d6fb20d3bfca23593
haldaniko <haldaniko@gmail.com>
2024-07-30 17:57:31 +0200

    fix secret key

--- train_station/settings.py
+++ train_station/settings.py
@@ -6,7 +6,7 @@ from dotenv import load_dotenv
 
 BASE_DIR = Path(__file__).resolve().parent.parent
 
-dotenv_path = Path(__file__).resolve().parent / '.env'
+dotenv_path = BASE_DIR / '.env'
 load_dotenv(dotenv_path=dotenv_path)
 
 SECRET_KEY = os.getenv('SECRET_KEY')
